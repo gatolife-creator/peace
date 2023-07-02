60f8f87511ff12d1824eaaa1fca34833233eb3e4
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-07-02 10:49:22 +0900

    Update scripts for contract deployment.

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,21 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
+if (process.env.PRIVATE_KEY === undefined) {
+  console.log('private key is missing');
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -4,17 +4,17 @@ const main = async () => {
   const PeaceStorageFactory = await ethers.getContractFactory("PeaceStorage");
   const PeaceStorage = await PeaceStorageFactory.deploy();
   console.log("PeaceStorage: ");
-  console.log("deployed at: " + PeaceStorage.getAddress());
+  console.log("deployed at: " + await PeaceStorage.getAddress());
 
   const PeacefulTokenFactory = await ethers.getContractFactory("PeacefulToken");
   const PeacefulToken = await PeacefulTokenFactory.deploy();
   console.log("PeacefulToken: ");
-  console.log("deployed at: " + PeacefulToken.getAddress());
+  console.log("deployed at: " + await PeacefulToken.getAddress());
 
   const PeaceFactory = await ethers.getContractFactory("Peace");
   const Peace = await PeaceFactory.deploy(PeaceStorage.getAddress(), PeacefulToken.getAddress());
   console.log("Peace: ");
-  console.log("deployed at: " + Peace.getAddress());
+  console.log("deployed at: " + await Peace.getAddress());
 }
 
 const runMain = async () => {
